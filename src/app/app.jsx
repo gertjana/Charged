febeb3a165e8c8f1eaa226db48aba10c28fbc6ee
Gertjan Assies <g.assies@thenewmotion.com>
2015-02-16 16:44:48 +0100

    added image for improvement

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -4,30 +4,32 @@
     Main = require('./components/main.jsx'); // Our custom react component
 
 
+var barColor = "rgba(128,160,255,1)";
+
 var chartData = {
     labels: ["16", "17", "18", "19", "20", "21", "22"],
     datasets: [
         {
-            fillColor: "rgba(128,160,255,1)",
-            strokeColor: "rgba(128,160,255,1)",
-            pointColor: "rgba(128,160,255,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(128,160,255,1)",
+            fillColor: barColor,
+            strokeColor: barColor,
+            pointColor: barColor,
+            pointStrokeColor: barColor,
+            pointHighlightFill: barColor,
+            pointHighlightStroke: barColor,
             data: [65, 59, 80, 81, 56, 55, 40]
         }
     ]
 };
 
 var data = [
-    {id:0, "itemtype":"badge",   "icon":"action-bookmark",   "title":"Improvement", "picture":"", "description":"You charged more today than yesterday, keep up the good work"},
-    {id:1, "itemtype":"session", "icon":"action-assessment", "title":"Charged Daily", "date":"05-02-2015", "kWh":20, "CO2":45},
-    {id:2, "itemtype":"session", "icon":"action-assessment", "title":"Charged Daily", "date":"05-02-2015", "kWh":13, "CO2":34},
+    {id:0, "itemtype":"badge",   "icon":"action-bookmark",   "title":"Improving...", "picture":"./images/improve.png", "description":"You charged more today than yesterday, keep up the good work"},
+    {id:1, "itemtype":"session", "icon":"action-assessment", "title":"Charged", "date":"26-03-2015", "kWh":20, "CO2":45},
+    {id:2, "itemtype":"session", "icon":"action-assessment", "title":"Charged", "date":"25-03-2015", "kWh":13, "CO2":34},
     {id:3, "itemtype":"badge",   "icon":"action-bookmark",   "title":"Drove around the world", "picture":"./images/globe.png", "description":"Congratulations, you charged over 8000 kWh. that is enough to drive around the world!"},
-    {id:4, "itemtype":"session", "icon":"action-assessment", "title":"Charged Daily", "date":"05-02-2015", "kWh":16, "CO2":37},
+    {id:4, "itemtype":"session", "icon":"action-assessment", "title":"Charged", "date":"24-03-2015", "kWh":16, "CO2":37},
     {id:5, "itemtype":"weekly",  "icon":"action-assessment", "title":"Charged Weekly", "week":"2015 12", kWh:234, "CO2":345, "data":chartData},
-    {id:6, "itemtype":"session", "icon":"action-assessment", "title":"Charged Daily", "date":"05-02-2015", "kWh":34, "CO2":78},
-    {id:7, "itemtype":"session", "icon":"action-assessment", "title":"Charged Daily", "date":"05-02-2015", "kWh":23, "CO2":51},
+    {id:6, "itemtype":"session", "icon":"action-assessment", "title":"Charged", "date":"22-03-2015", "kWh":34, "CO2":78},
+    {id:7, "itemtype":"session", "icon":"action-assessment", "title":"Charged", "date":"21-03-2015", "kWh":23, "CO2":51},
   ]
 
   //Needed for React Developer Tools
