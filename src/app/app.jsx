445449e96d44fc8ae7add2a31e4e412538ac8aa1
Gertjan Assies <g.assies@thenewmotion.com>
2015-02-16 14:47:03 +0100

    fixed image locations for mobile

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -23,7 +23,7 @@ var data = [
     {id:0, "itemtype":"badge",   "icon":"action-bookmark",   "title":"Improvement", "picture":"", "description":"You charged more today than yesterday, keep up the good work"},
     {id:1, "itemtype":"session", "icon":"action-assessment", "title":"Charged Daily", "date":"05-02-2015", "kWh":20, "CO2":45},
     {id:2, "itemtype":"session", "icon":"action-assessment", "title":"Charged Daily", "date":"05-02-2015", "kWh":13, "CO2":34},
-    {id:3, "itemtype":"badge",   "icon":"action-bookmark",   "title":"Drove around the world", "picture":"/www/images/globe.png", "description":"Congratulations, you charged enough kWh to drive around the world!"},
+    {id:3, "itemtype":"badge",   "icon":"action-bookmark",   "title":"Drove around the world", "picture":"./images/globe.png", "description":"Congratulations, you charged over 8000 kWh. that is enough to drive around the world!"},
     {id:4, "itemtype":"session", "icon":"action-assessment", "title":"Charged Daily", "date":"05-02-2015", "kWh":16, "CO2":37},
     {id:5, "itemtype":"weekly",  "icon":"action-assessment", "title":"Charged Weekly", "week":"2015 12", kWh:234, "CO2":345, "data":chartData},
     {id:6, "itemtype":"session", "icon":"action-assessment", "title":"Charged Daily", "date":"05-02-2015", "kWh":34, "CO2":78},
